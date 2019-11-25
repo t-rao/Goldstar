59e76bbfb76ff278e10a0766aa320818aec4f2d4
t-rao <traodesigner@gmail.com>
2019-11-26 00:14:59 +0530

    Updates

--- js/script.js
+++ js/script.js
@@ -126,3 +126,89 @@ $(document).ready(function () {
     $("img.lazy").lazyload();
 
 });
+
+$(window).on("scroll", function () {
+
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            if ($(window).scrollTop() > 50) {
+                $(".header2").addClass("active");
+                $(".icon2 a").css("color", "black");
+                $(".nav_item2 a").css("color", "black");
+                $(".nav_item2 a span").css("color", "black");
+                $(".nav_item2 a i").css("color", "black");
+                $(".logo2").css("color", "black");
+                $(".overlay2").css("background", "White");
+                $("#closebtn2").css("color", "black");
+                $(' #free_btn2 ').css("color", "white");
+                $(' #free_btn2 ').css({
+                    "border-color": "#000000",
+                    "border-width": "1px",
+                    "border-style": "solid"
+                });
+                $('#logo2').attr("src", "images/logo_scrool.png");
+            } else {
+                //remove the background property so it comes transparent again (defined in your css)
+                $(".header2").removeClass("active");
+                $(".icon2 a").css("color", "black");
+                $(".nav_item2 a").css("color", "White");
+                $(".nav_item2 a span").css("color", "white");
+                $(".nav_item2 a i").css("color", "white");
+                $(".logo2").css("color", "White");
+                $(".overlay2").css("background", "black");
+                $("#closebtn2").css("color", "white!important");
+                $(' #free_btn2 ').css("color", "white");
+                $(' #free_btn2 ').css({
+                    "border-color": "#ffffff",
+                    "border-width": "1px",
+                    "border-style": "solid"
+                });
+                $('#logo2').attr("src", "images/logo_scrool.png");
+            }
+        } else {
+            if ($(window).scrollTop() > 50) {
+                $(".header2").addClass("active");
+                $(".icon2 a").css("color", "black");
+                $(".nav_item2 a").css("color", "black");
+                $(".nav_item2 a span").css("color", "black");
+                $(".nav_item2 a i").css("color", "black");
+                $(".overlay2").css("background", "White");
+                $(".logo2").css("color", "black");
+                $("#closebtn2").css("color", "black");
+                $(' #free_btn2 ').css("color", "white");
+                $(' #free_btn2 ').css({
+                    "border-color": "#000000",
+                    "border-width": "1px",
+                    "border-style": "solid"
+                });
+                $('#logo2').attr("src", "images/logo_scrool.png");
+            } else {
+                //remove the background property so it comes transparent again (defined in your css)
+                $(".header2").removeClass("active");
+                $(".icon2 a").css("color", "black");
+                $(".nav_item2 a").css("color", "black");
+                $(".nav_item2 a span").css("color", "black");
+                $(".nav_item2 a i").css("color", "black");
+                $(".overlay2").css("background", "black");
+                $(".logo2").css("color", "White");
+                $("#closebtn2").css("color", "white");
+                $(' #free_btn2 ').css("color", "white");
+                $(' #free_btn2 ').css({
+                    "border-color": "#000000",
+                    "border-width": "1px",
+                    "border-style": "solid"
+                });
+                $('#logo2').attr("src", "images/logo_scrool.png");
+            }
+        }
+    }
+
+    var x = window.matchMedia("(max-width: 500px)")
+
+    myFunction(x) // Call listener function at run time
+
+    x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+});
